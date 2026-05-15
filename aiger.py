1b5d6507b7df249d3313779fa20ada2fd5282257
Ege Tekin <et130@students.uni-freiburg.de>
2026-05-15 13:45:45 +0200

    Move AIGER modules into package

--- aiger.py
+++ aiger.py
@@ -1,10 +1,1 @@
-from netlist_builder import NetlistBuilder
-from aiger_writer import AigerWriter
-
-
-def compile_expr_to_aiger(expr):
-    """Compile a logical expression into ASCII AIGER."""
-    builder = NetlistBuilder()
-    output_id = builder.compile_expr(expr)
-    writer = AigerWriter(builder.nodes, output_id)
-    return writer.write()
+from string_to_aiger.aiger.aiger import *
---
+++ string_to_aiger/aiger/aiger.py
@@ -0,0 +1,10 @@
+from netlist_builder import NetlistBuilder
+from .aiger_writer import AigerWriter
+
+
+def compile_expr_to_aiger(expr):
+    """Compile a logical expression into ASCII AIGER."""
+    builder = NetlistBuilder()
+    output_id = builder.compile_expr(expr)
+    writer = AigerWriter(builder.nodes, output_id)
+    return writer.write()
